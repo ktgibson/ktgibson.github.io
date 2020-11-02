862102c578b9b2ec61a99837af528de6c6cdaf7f
ktgibson <ktgibson0414@gmail.com>
2020-11-02 13:02:52 -0500

    refactor code to fix issue #1: last guess not in range

--- numberGen.js
+++ numberGen.js
@@ -15,19 +15,30 @@ function init(){
     guessBtn.onclick = function() {numberGen()};
 
     function numberGen(){
-        if(guessArr.length < 9){ // if length of guesses array is < 10
-            if(userInput.value >= 0 && userInput.value <= 50){ //if the input is within range
+            if(userInput.value >= 0 && userInput.value <= 50 && userInput.value != ''){ //if the input is within range
                 if(userInput.value != rando){ // if the number was not just guesses
                     if(!guessArr.includes(userInput.value)){ //check if array has duplicate value
-                        guessArr.push(userInput.value); // add user input to array
-                        guessArea.innerText = guessArr; //print whole array on text area
-                        title.innerText = "Guess the Number between 1 & 50"; //reset the title
-                        triesRemaining -= 1; //subtract 1 from triesRemaining
-                        triesLeft.innerText = String(triesRemaining);//set tries remaining
-                        userInput.value = ""; //reset userinput
-                        userInput.focus(); //focus textbox
-                        colorChange(); //change color based on tried left
+                        if(guessArr.length < 9){ // if length of guesses array is < 10
+                            guessArr.push(userInput.value); // add user input to array
+                            guessArea.innerText = guessArr; //print whole array on text area
+                            title.innerText = "Guess the Number between 1 & 50"; //reset the title
+                            triesRemaining -= 1; //subtract 1 from triesRemaining
+                            triesLeft.innerText = String(triesRemaining);//set tries remaining
+                            userInput.value = ""; //reset userinput
+                            userInput.focus(); //focus textbox
+                            colorChange(); //change color based on tried left
                     } else{
+                            createResetButton();
+                            guessArr.push(userInput.value); //push last guess
+                            guessArea.innerText = guessArr; //set last guess to area text
+                            title.innerText = (`Stupid human.. The number was ${rando}`); //notify player of the number
+                            title.style.color = "red";
+                            triesLeft.innerText = "0"; //set tries to 0
+                            userInput.value = '';
+                            userInput.setAttribute("placeholder", "You're a failure...");
+                            guessBtn.disabled = true; //disable submit button
+                    }
+                    }else{
                         title.innerText = "NO DUPLICATES!"; //throw error if duplicate number
                     }
                 }else{
@@ -40,17 +51,6 @@ function init(){
             }else{
                 title.innerText = ("ERROR: INVALID NUMBER"); //enter number in range
             }
-        }else{
-            createResetButton();
-            guessArr.push(userInput.value); //push last guess
-            guessArea.innerText = guessArr; //set last guess to area text
-            title.innerText = (`Stupid human.. The number was ${rando}`); //notify player of the number
-            title.style.color = "red";
-            triesLeft.innerText = "0"; //set tries to 0
-            userInput.value = '';
-            userInput.setAttribute("placeholder", "You're a failure...");
-            guessBtn.disabled = true; //disable submit button
-        }
     }
 
     function createResetButton(){
